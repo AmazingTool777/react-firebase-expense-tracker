03052452eaae5774349abb01d1d6864355358c79
Tolojanahary <tolotor@yahoo.fr>
2024-02-12 14:08:06 +0300

    Firebase emulators setup

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,7 +1,15 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { GoogleAuthProvider, getAuth } from "firebase/auth";
-import { collection, getFirestore } from "firebase/firestore";
+import {
+  GoogleAuthProvider,
+  connectAuthEmulator,
+  getAuth,
+} from "firebase/auth";
+import {
+  collection,
+  connectFirestoreEmulator,
+  getFirestore,
+} from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -20,6 +28,10 @@ export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const googleAuthProvider = new GoogleAuthProvider();
 export const db = getFirestore(app);
+
+// Local emultors settings
+connectAuthEmulator(auth, "http://127.0.0.1:9099");
+connectFirestoreEmulator(db, "127.0.0.1", 8082);
 
 export const collectionsRefs = {
   users: collection(db, "users"),
